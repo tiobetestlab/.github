73483b573b95ef0766cf2ec26ea7189fe663e4c9
marvinwener <45437696+marvinwener@users.noreply.github.com>
2021-06-02 13:04:48 +0200

    Update index.js

--- actions/tics/index.js
+++ actions/tics/index.js
@@ -16,10 +16,12 @@ async function analyseTiCSBranch() {
     try {
         console.log(`Analysing new pull request for project ${ticsConfig.projectName} and ${ticsConfig.branchName} and ${ticsConfig.branchDir}.`)
         
-        execString = '-qg '
+        execString = 'TICS -qg '
         execString =+ ticsConfig.projectName ? `-project ${ticsConfig.projectName} ` : '';
         execString =+ ticsConfig.viewerToken ? `-cdtoken ${ticsConfig.viewerToken} ` : '';
         execString =+ 'tics_dock.py';
+        
+        console.log(execString);
         
         exec(execString, (error, stdout, stderr) => {
             if (error || stderr) {
