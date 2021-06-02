2fe35f93001c0e89cf29c8e339a0b13d5ff87c2b
marvinwener <45437696+marvinwener@users.noreply.github.com>
2021-06-02 12:52:32 +0200

    Update index.js

--- actions/tics/index.js
+++ actions/tics/index.js
@@ -38,7 +38,7 @@ async function analyseTiCSBranch() {
             }
 
             console.log(stdout)
-            createPrComment();
+            //createPrComment();
             
         });
 
